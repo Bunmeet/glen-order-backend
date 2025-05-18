263506d54ac8311e930d60ba9f5b4563de4b1e6d
Bunmeet <bunmeet@houzoldz.co.in>
2025-05-18 18:09:36 +0530

    Create orderController.js

---
+++ routes/controllers/orderController.js
@@ -0,0 +1,20 @@
+const Order = require('../models/Order');
+
+exports.createOrder = async (req, res) => {
+  try {
+    const order = new Order(req.body);
+    await order.save();
+    res.status(201).json(order);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating order', error });
+  }
+};
+
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching orders', error });
+  }
+};
